66eb7b2f4fef8cde9552271c6fa12deadc94e955
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-02-21 21:24:05 +0600

    started work on search function

---
+++ main.js
@@ -0,0 +1,19 @@
+// Declaring constants
+const container = document.querySelector('.container');
+const searchButton = document.querySelector('.search-box button');
+const weatherPrimary = document.querySelector('.weather-primary');
+const weatherSecondary = document.querySelector('.weather-secondary');
+const errorPage = document.querySelector('.error-page');
+
+// Search function on button click
+searchButton.addEventListener('click', () =>{
+    // APIKey
+    const APIKey = 'dummy-value';
+    // Getting City name input from user
+    const cityName = document.querySelector('search-box input').value;
+
+    // Checking for empty input
+    if(cityName === " "){
+        return;
+    }
+})
